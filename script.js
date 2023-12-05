561e28f350b3a78d540606855b64cddbf33faa1c
olaide <holaryidealabi@gmail.com>
2023-12-05 10:24:36 -0800

    updated home page

---
+++ script.js
@@ -0,0 +1,16 @@
+const mainMenu = document.querySelector('.main-menu');
+const openMenu = document.querySelector('.menu');
+const closeMenu = document.querySelector('.closemenu');
+
+openMenu.addEventListener('click', open);
+closeMenu.addEventListener('click', close);
+
+
+function open() {
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+
+function close() {
+    mainMenu.style.top = '-100vh';
+}
